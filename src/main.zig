01c9bd330d9a650c6943f7838b1fb502d82e4ce7
Linghua Zhang <linghua.zhang@me.com>
2025-02-09 01:37:39 +0900

    feat: verbose mode

--- src/main.zig
+++ src/main.zig
@@ -4,27 +4,49 @@ const builtin = @import("builtin");
 const ver = @import("version.zig");
 const server = @import("server_cmd.zig");
 
+var verbose: bool = false;
+
 pub fn main() !void {
     var buf: [1024]u8 = undefined;
     var fba = std.heap.FixedBufferAllocator.init(&buf);
     const allocator = fba.allocator();
 
     var r = try cli.AppRunner.init(allocator);
-    const app = cli.App{ .command = cli.Command{ .name = "toyskkserv", .target = cli.CommandTarget{
-        .subcommands = &[_]cli.Command{
-            .{
-                .name = "serve",
-                .target = cli.CommandTarget{
-                    .action = cli.CommandAction{ .exec = server.serve },
-                },
-            },
-            .{
-                .name = "version",
-                .target = cli.CommandTarget{
-                    .action = cli.CommandAction{ .exec = ver.showVersion },
+    const app = cli.App{
+        .command = cli.Command{
+            .name = "toyskkserv",
+            .target = cli.CommandTarget{
+                .subcommands = &[_]cli.Command{
+                    .{
+                        .name = "serve",
+                        .target = cli.CommandTarget{
+                            .action = cli.CommandAction{ .exec = serve },
+                        },
+                        .options = &[_]cli.Option{
+                            .{
+                                .long_name = "verbose",
+                                .short_alias = 'v',
+                                .help = "Enable more output",
+                                .value_ref = r.mkRef(&verbose),
+                            },
+                        },
+                    },
+                    .{
+                        .name = "version",
+                        .target = cli.CommandTarget{
+                            .action = cli.CommandAction{ .exec = ver.showVersion },
+                        },
+                    },
                 },
             },
         },
-    } } };
+    };
     return r.run(&app);
+}
+
+fn serve() !void {
+    if (verbose) {
+        @import("log.zig").setLevel(.debug);
+    }
+    try server.serve();
 }
